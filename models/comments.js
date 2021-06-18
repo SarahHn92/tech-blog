77d4270cd83411211a19e3d484ebb9eefd877619
Sarah Hinton <smcfarland2@gmail.com>
2021-06-18 15:01:10 +1000

    adding Comments model

---
+++ models/comments.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+Comments.init(
+    {
+        id: {
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        date : {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            validate: {
+                isDate: true
+            }
+        },
+        body: {
+            type: DataTypes.STRING
+
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    }
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments'
+      }
+);
+
+module.exports = Comments;
+
