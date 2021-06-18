40aefca10bba0b54536c454fb45990cdd1fe9e67
Sarah Hinton <smcfarland2@gmail.com>
2021-06-18 15:53:28 +1000

    adding connection.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'secrecy and privacy',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
