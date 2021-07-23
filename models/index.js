4b699d63ed9c70edace2d2e73757b8098e2b06c8
Sarah Hinton <smcfarland2@gmail.com>
2021-07-23 22:58:21 +1000

    adding pseudocode to index.js for associations

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+
+// relationships and associations
+
+// User has many posts
+
+// Post belongs to User
+
+// User has many comments
+
+// Comment belongs to Post 
+
+// Relationship between User and Comment?
+
