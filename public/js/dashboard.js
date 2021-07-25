d50718b3f85c08eabebaf7ce975adaaf83012d82
Sarah Hinton <smcfarland2@gmail.com>
2021-07-25 21:33:14 +1000

    adding dashboard functionality

---
+++ public/js/dashboard.js
@@ -0,0 +1,7 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#blogName').value.trim();
+    const body= document.querySelector('#blogBody').value.trim();
+    const description = document.querySelector('#blogDesc').value.trim();
+}
