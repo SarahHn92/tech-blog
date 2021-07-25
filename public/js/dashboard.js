3d702e0e7d9e9487bc13cd6bf944c86806c11cd7
Sarah Hinton <smcfarland2@gmail.com>
2021-07-25 21:42:36 +1000

    adding formHandler to dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,4 +4,20 @@ const newFormHandler = async (event) => {
     const name = document.querySelector('#blogName').value.trim();
     const body= document.querySelector('#blogBody').value.trim();
     const description = document.querySelector('#blogDesc').value.trim();
-}
+
+    if (name && body && description) {
+        const response = await fetch(`/api/dashboard`, {
+            method: 'POST',
+            body: JSON.stringify({ name, body, description }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Blogpost failed to compile!');
+        }
+    }
+};
