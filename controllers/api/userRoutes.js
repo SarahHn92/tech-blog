3f7a4e04f6dab3ade94108c10fedbecf3abeecb8
Sarah Hinton <smcfarland2@gmail.com>
2021-08-01 19:54:44 +1000

    beginning userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,2 +1,17 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+      const userInfo = await User.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = userInfo.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userInfo);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+});
