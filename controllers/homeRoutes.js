cedb9dbe99943299034a26b3503f7cbd2262efe5
Sarah Hinton <smcfarland2@gmail.com>
2021-08-01 19:50:20 +1000

    Adding postRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -56,9 +56,8 @@ router.get('/login', (req, res) => {
   });
   
   router.get('/signup', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/profile');
+      res.redirect('/dash');
       return;
     }
   
