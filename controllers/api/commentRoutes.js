ef823650c7b66c5ae4cc4a58e0948d6c8f4c56a5
Sarah Hinton <smcfarland2@gmail.com>
2021-08-03 18:13:50 +1000

    fixing errors

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { any } = require('sequelize/types/lib/operators');
 const { Comment } =require('../../models/comment');
 const withAuth = require('../../utils/auth');
 
